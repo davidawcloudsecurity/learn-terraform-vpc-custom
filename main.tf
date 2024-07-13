aba9e82fc0c0fcbacacf8a1dc2b3e8caa646ccb7
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-07-13 12:44:10 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ variable "create_instance_pod" {
 variable "user_tags" {
   description = "Please provide a name for your project. It will also be used to tag resources"
   type        = string
-  default     = example-01
+  default     = "example-01"
 }
 
 variable "resource_tags" {
