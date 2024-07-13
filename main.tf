fd181ae1f2b0d74ee369b450c207e91902a3a105
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-07-13 18:26:22 +0800

    Update main.tf - added port 80 allow in sg

--- main.tf
+++ main.tf
@@ -255,6 +255,14 @@ resource "aws_security_group" "public" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow HTTP access from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
